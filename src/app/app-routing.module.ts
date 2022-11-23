bea70bb71604b2168f80bc8e3ab7f67e368a64f6
Ted Pritchard <ted.pritchard1@hmcts.net>
2022-11-23 17:28:52 +0000

    add intro component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: '',
+    loadChildren: () => import('./space-tourism/space-tourism.routes'),
+  },
+  {
     path: 'design-system',
     loadChildren: () => import('./design-system/design-system.routes'),
   },
---
+++ src/app/space-tourism/space-tourism.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { IntroComponent } from './intro/intro.component';
+
+export default [{ path: '', component: IntroComponent }] as Routes;
