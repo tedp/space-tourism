56f79b18770e5446f9c72c911d120570cdb45a7b
Ted Pritchard <ted.pritchard1@hmcts.net>
2022-11-23 10:45:19 +0000

    WIP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'design-system',
+    loadChildren: () =>
+      import('./design-system/design-system.module').then(
+        (m) => m.DesignSystemModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
