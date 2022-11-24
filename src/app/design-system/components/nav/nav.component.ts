49453ddb79009cc19b443341d345b671acec381f
Ted Pritchard <ted.pritchard1@hmcts.net>
2022-11-24 09:47:00 +0000

    Initial setup of intro page

--- src/app/design-system/components/nav/nav.component.ts
+++ src/app/design-system/components/nav/nav.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+export interface NavLink {
+  link: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
-
+  @Input() navLinks: NavLink[] = [];
 }
--- src/app/space-tourism/space-tourism.component.ts
+++ src/app/space-tourism/space-tourism.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {
+  NavComponent,
+  NavLink,
+} from '../design-system/components/nav/nav.component';
 
 @Component({
   selector: 'app-space-tourism',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NavComponent],
   templateUrl: './space-tourism.component.html',
   styleUrls: ['./space-tourism.component.scss'],
 })
-export class SpaceTourismComponent {}
+export class SpaceTourismComponent {
+  navLinks: NavLink[] = [
+    { title: 'Home', link: 'intro' },
+    { title: 'Destinations', link: 'destinations' },
+    { title: 'Crew', link: 'crew' },
+    { title: 'Technology', link: 'technology' },
+  ];
+}
