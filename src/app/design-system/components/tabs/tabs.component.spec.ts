22d7f922a5ed68b990a780af8df280915bf78351
Ted Pritchard <ted.pritchard1@hmcts.net>
2022-11-25 17:21:04 +0000

    Tab component

--- src/app/design-system/components/tabs/tabs.component.spec.ts
+++ src/app/design-system/components/tabs/tabs.component.spec.ts
@@ -1,23 +1,65 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TabsComponent } from './tabs.component';
+import { TabsComponent, Tab } from './tabs.component';
+import { By } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 describe('TabsComponent', () => {
+  let testHostComponent: TestHostComponent;
   let component: TabsComponent;
-  let fixture: ComponentFixture<TabsComponent>;
+  let fixture: ComponentFixture<TestHostComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ TabsComponent ]
-    })
-    .compileComponents();
+      imports: [TabsComponent],
+      declarations: [TestHostComponent],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(TabsComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(TestHostComponent);
+    testHostComponent = fixture.componentInstance;
+    component = fixture.debugElement.query(
+      By.directive(TabsComponent)
+    ).componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('selected navigation', () => {
+    it('when initialise the selected tab index is 1', () => {
+      expect(component.selectedTabIndex).toEqual(1);
+    });
+
+    it('when tab selected should emit selected tab name', () => {
+      jest.spyOn(component.selectedTabChanged, 'emit');
+      const tabItems = fixture.debugElement.queryAll(By.css('.tablist button'));
+
+      tabItems[0].nativeElement.click();
+      fixture.detectChanges();
+
+      expect(tabItems.length).toEqual(2);
+      expect(component.selectedTabChanged).toHaveBeenCalledWith('tab1');
+    });
+  });
 });
+
+@Component({
+  selector: 'test-host',
+  template: `
+    <app-tabs
+      [tabs]="tabs"
+      [selectedTabIndex]="selectedTabIndex"
+      (selectedTabChanged)="onSelectedTabChanged($event)"
+    ></app-tabs>
+  `,
+})
+export class TestHostComponent {
+  selectedTabIndex = 1;
+  navLinks: Tab[] = [{ name: 'tab1' }, { name: 'tab2' }];
+
+  onSelectedNavChanged(selectedTabIndex: number) {
+    this.selectedTabIndex = selectedTabIndex;
+  }
+}
--- src/app/design-system/components/tabs/tabs.component.ts
+++ src/app/design-system/components/tabs/tabs.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+export interface Tab {
+  name: string;
+}
 @Component({
   selector: 'app-tabs',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.scss']
+  styleUrls: ['./tabs.component.scss'],
 })
 export class TabsComponent {
+  @Input() tabs: Tab[] = [];
+  @Input() selectedTabIndex = 0;
 
+  @Output() selectedTabChanged = new EventEmitter<string>();
 }
