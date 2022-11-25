1ddb4dc832e7d88823a5eead547393bd4aec3142
Ted Pritchard <ted.pritchard1@hmcts.net>
2022-11-25 16:29:49 +0000

    WIP

--- src/app/space-tourism/destinations/destinations.component.ts
+++ src/app/space-tourism/destinations/destinations.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContentServiceService } from '../services/content-service.service';
 
 @Component({
   selector: 'app-destinations',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './destinations.component.html',
-  styleUrls: ['./destinations.component.scss']
+  styleUrls: ['./destinations.component.scss'],
 })
 export class DestinationsComponent {
-
+  constructor(public contentService: ContentServiceService) {}
 }
