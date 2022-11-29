09399124811afa2410abe5ede3d35dfecae77ad3
Ted Pritchard <ted.pritchard1@hmcts.net>
2022-11-29 10:15:06 +0000

    Moon background

--- src/app/space-tourism/space-tourism.component.ts
+++ src/app/space-tourism/space-tourism.component.ts
@@ -16,17 +16,10 @@ import { filter, map, Observable, tap } from 'rxjs';
   styleUrls: ['./space-tourism.component.scss'],
 })
 export class SpaceTourismComponent {
-  selectedNavIndex$: Observable<number>;
-
   constructor(
     private contenService: ContentServiceService,
     private router: Router
-  ) {
-    this.selectedNavIndex$ = router.events.pipe(
-      filter((event: any) => event instanceof NavigationStart),
-      map((event: NavigationStart) => this.getSelectedNavIndex(event.url))
-    );
-  }
+  ) {}
 
   navLinks: NavLink[] = [
     { title: 'Home', link: '', exact: true },
@@ -35,20 +28,10 @@ export class SpaceTourismComponent {
     { title: 'Technology', link: 'technology', exact: false },
   ];
 
-  getSelectedNavIndex(url: string): number {
-    return this.navLinks.findIndex(
-      (navLink) => navLink.link === url.substring(1)
-    );
-  }
-
-  getHomeClass(selectedNavIndex: number | null): string {
-    let navIndex: number;
-    if (selectedNavIndex && selectedNavIndex >= 0) {
-      navIndex = selectedNavIndex;
-    } else {
-      navIndex = this.getSelectedNavIndex(this.router.routerState.snapshot.url);
-    }
-    return '';
-    // return `home--${(this, this.navLinks[navIndex].link)}`;
+  getHomeClass(): string {
+    const parialClassName = this.router.routerState.snapshot.url
+      .split('/')
+      .join('-');
+    return `home-${parialClassName}`;
   }
 }
