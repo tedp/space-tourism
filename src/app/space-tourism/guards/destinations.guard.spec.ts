3031e6e51af2319f41451b48b63ba2c6f58f1ee0
Ted Pritchard <ted.pritchard1@hmcts.net>
2022-12-01 10:33:12 +0000

    fix tests

--- src/app/space-tourism/guards/destinations.guard.spec.ts
+++ src/app/space-tourism/guards/destinations.guard.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DestinationsGuard } from './destinations.guard';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('DestinationsGuard', () => {
   let guard: DestinationsGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ providers: [provideMockStore()] });
     guard = TestBed.inject(DestinationsGuard);
   });
 
